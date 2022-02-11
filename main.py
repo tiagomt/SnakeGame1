56b0536217b47010a6ca8878415a8adc7836d734
Tiagomt <tiago-tmt@hotmail.com>
2022-02-11 14:32:43 -0300

    Movimentaçao da snake e inserçao da Apple

--- main.py
+++ main.py
@@ -3,16 +3,34 @@ import pygame
 from pygame.locals import *
 import time
 
+SIZE = 40
+
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("./resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE * 3
+        self.y = SIZE * 3
+
+    def draw(self):
+        # joga block no parent_screen(tela do game)
+        self.parent_screen.blit(self.image, (self.x, self.y))
+
+        # atualizando tela
+        pygame.display.flip()
+
 
 class Snake:
 
-    def __init__(self, parent_screen):
+    def __init__(self, parent_screen, length):
 
+        self.length = length
         self.parent_screen = parent_screen
         # carregando imagem "bloco"
         self.block = pygame.image.load("./resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         # direção padrão
         self.direction = "down"
 
@@ -20,8 +38,10 @@ class Snake:
         # self.parent_screen.fill irá "apagar" blocos anteriores
         self.parent_screen.fill((110, 110, 5))
 
-        # joga block no parent_screen(tela do game)
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        # for para jogar blocos na tela
+        for i in range(self.length):
+            # joga block no parent_screen(tela do game)
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
 
         # atualizando tela
         pygame.display.flip()
@@ -39,17 +59,24 @@ class Snake:
         self.direction = "down"
 
     def walk(self):
+
+        # definindo posição do bloco como bloco anterior
+        for i in range(self.length - 1, 0, -1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
+        # movimento do tamnanho do bloco(SIZE)
         if self.direction == "left":
-            self.x -= 10
+            self.x[0] -= SIZE
 
         if self.direction == "right":
-            self.x += 10
+            self.x[0] += SIZE
 
         if self.direction == "up":
-            self.y -= 10
+            self.y[0] -= SIZE
 
         if self.direction == "down":
-            self.y += 10
+            self.y[0] += SIZE
 
         self.draw()
 
@@ -60,16 +87,26 @@ class Game:
         pygame.init()
 
         # iniciando a janela do jogo com 1000x500
-        self.surface = pygame.display.set_mode((500, 500))
+        self.surface = pygame.display.set_mode((1000, 800))
 
         # definindo cor
         self.surface.fill((110, 110, 5))
 
         # definindo snake dentro do game passando a tela do game
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 6)
 
         # jogando block na tela do game
         self.snake.draw()
+
+        # definindo apple dentro do game passando a tela do game
+        self.apple = Apple(self.surface)
+
+        # jogando apple na tela do game
+        self.apple.draw()
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
 
     def run(self):
         running = True
@@ -100,8 +137,8 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
-            time.sleep(0.2)
+            self.play()
+            time.sleep(0.3)
 
 
 if __name__ == "__main__":
